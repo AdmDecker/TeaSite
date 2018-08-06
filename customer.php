f1789658dfdfb9d041afb58ffa9a157a642f4cca
Adam Decker <deflect57@live.com>
2018-08-06 15:57:07 -0500

    Update customer.php

--- customer.php
+++ customer.php
@@ -5,17 +5,20 @@
 ?>
 
 <!DOCTYPE html>
-<script>
-    function requestTea() {
-        document.getElementById('requestTea').enabled = false;
-        let action = '/requestTea.php';
-        xmlhttp = new XMLHttpRequest();
-        //Open our http request as POST with our action variable
-        xmlhttp.open("POST", action, true);
-        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp.send('requestMessage="My Message"');
-    }
-</script>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <script>
+        function requestTea() {
+            document.getElementById('requestTea').enabled = false;
+            let action = '/requestTea.php';
+            xmlhttp = new XMLHttpRequest();
+            //Open our http request as POST with our action variable
+            xmlhttp.open("POST", action, true);
+            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xmlhttp.send('requestMessage="My Message"');
+        }
+    </script>
+</head>
 <html>
     <head>
         <link href="w3.css" rel="stylesheet" type="text/css">
