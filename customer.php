9adb311f2fc6a79dfbdbce7c6ee8c335bc22b81f
Adam Decker <deflect57@live.com>
2018-08-06 13:31:04 -0500

    Update customer.php

--- customer.php
+++ customer.php
@@ -5,6 +5,17 @@
 ?>
 
 <!DOCTYPE html>
+<script>
+    function requestTea() {
+        document.getElementById('requestTea').enabled = false;
+        let action = '/requestTea.php';
+        xmlhttp = new XMLHttpRequest();
+        //Open our http request as POST with our action variable
+        xmlhttp.open("POST", action, true);
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xmlhttp.send();
+    }
+</script>
 <html>
     <head>
         <link href="w3.css" rel="stylesheet" type="text/css">
@@ -13,5 +24,6 @@
     <body>
         <h2>Welcome <?php echo PupSession::getUsername() ?>,</h2>
         <p>You have <?php echo $teas ?> teas</p>
+        <button id='requestTea' onclick="requestTea()">Order Tea</button>
     </body>
 </html>
