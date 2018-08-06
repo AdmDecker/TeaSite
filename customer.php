919a2de0d0f53aa4dfc53d71a141b239569dd423
Adam Decker <deflect57@live.com>
2018-08-06 10:17:13 -0500

    Update customer.php

--- customer.php
+++ customer.php
@@ -10,7 +10,6 @@
         <link href="w3.css" rel="stylesheet" type="text/css">
         <link href="style.css" rel="stylesheet" type="text/css">
     </head>
-    <style
     <body>
         <h2>Welcome <?php echo PupSession::getUsername() ?>,</h2>
         <p>You have <?php echo $teas ?> teas</p>
