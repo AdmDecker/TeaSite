7f74d293e2f99e43c0630cd3f077364dedb5cdd0
Adam Decker <deflect57@live.com>
2018-08-06 13:27:24 -0500

    Create requestTea.php

---
+++ requestTea.php
@@ -0,0 +1,19 @@
+<?php
+	require_once('Session.php');
+	if (PupSession::getUserType != 'C')
+	{
+		echo 'fail';
+		exit();
+	}
+	
+	$requestMessage = $_POST['requestMessage'];
+	$to = '';
+	$subject = 'Tea Order';
+	$orderer = PupSession::getUsername();
+	$message = "<p>$orderer has requested a tea.</p><p>Request:</p><p>$requestMessage</p>";
+	$headers = "From: orders@t.pupperino.net";
+	
+	mail($to, $subject, $message, $headers);
+
+
+?>
