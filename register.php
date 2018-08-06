cdf05601d500e2dce008fd16cb8ba9194b9f2bac
Adam Decker <deflect57@live.com>
2018-08-06 13:37:13 -0500

    Update register.php

--- register.php
+++ register.php
@@ -2,31 +2,38 @@
     require 'dbaccess.php';
     $password = $_POST['password'];
     $username = trim($_POST['username']);
+    //Check Registercode
+    $registerCode = $_POST['registerCode']);
+    if ($registerCode != 'puppertea');
+    {
+		echo 'Incorrect registration code';
+		exit();
+    }
 
 	try {
-        //Create our database object
-        $db = new dbaccess();
-        
-        //Check if the user already exists.
-        $user_inDB = $db->getUserID($username);
+	//Create our database object
+	$db = new dbaccess();
+
+	//Check if the user already exists.
+	$user_inDB = $db->getUserID($username);
 		if (!is_null($user_inDB))
-        {
-            echo "User already exists";
-            exit();
-        }
-        
-        //Insert user to database
-        $db->addUser($username, $password, "C");
-        
-        //Get userID
-        $userID = $db->getUserID($username);
-        
-        session_start();
-        $_SESSION['username'] = $username;
-        $_SESSION['timeout'] = time() + 60*60*12;
-        $_SESSION['userID'] = $userID;
-        $_SESSION['userType'] = $db->getUserType($userID);
-        echo "success-register";
+	{
+	    echo "User already exists";
+	    exit();
+	}
+
+	//Insert user to database
+	$db->addUser($username, $password, "C");
+
+	//Get userID
+	$userID = $db->getUserID($username);
+
+	session_start();
+	$_SESSION['username'] = $username;
+	$_SESSION['timeout'] = time() + 60*60*12;
+	$_SESSION['userID'] = $userID;
+	$_SESSION['userType'] = $db->getUserType($userID);
+	echo "success-register";
 	}
 	catch(PDOException $e)
 	{
