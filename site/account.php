1e061b9b070d91748d14b9f027dbb9ec910d4536
Adam Decker <deflect57@live.com>
2018-08-08 10:10:37 -0500

    Update account.php

--- site/account.php
+++ site/account.php
@@ -16,25 +16,45 @@
                     return;
                 
                 if (xmlhttp.responseText.includes('success'))
-                    document.getElementById('passwordRequestError').innerHTML = 'Password change successful'
-                else
+                    document.getElementById('passwordRequestError').innerHTML = 'Password change successful';
+                else if(xmlhttp.responseText.includes('notification'))
+                    document.getElementById('notificationError').innerHTML = xmlhttp.responseText;
+                else if(xmlhttp.responseText.includes('password'))
                     document.getElementById('passwordRequestError').innerHTML = xmlhttp.responseText;
                     
             }
 
-            function submitPasswordRequest() {
-                let action = '/requestNewPass.php';
-                let oldPass = 'oldPass=' + document.getElementById('oldPass').value;
-                let newPass = 'newPass=' + document.getElementById('newPass').value;
-                xmlhttp = new XMLHttpRequest();
+            function submitForm(formSection) {
+                let action = '/saveAccountSettings.php';
+                let submitData = 'form=' + formSection;
                 
+                if (formSection === 'password') {
+                    let oldPass = 'oldPass=' + document.getElementById('oldPass').value;
+                    let newPass = 'newPass=' + document.getElementById('newPass').value;
+                    submitData += '&' + oldPass + '&' + newPass;
+                    document.getElementById("passwordRequestError").innerHTML = "Sending password request...";
+                }
+                else if (formSection === 'notification') {
+                    let email = 'email=' + document.getElementById('email');
+                    submitData += '&' + email;
+                    document.getElementById("notificationError").innerHTML = "Submitting notification settings...";
+                }
+                
+                xmlhttp = new XMLHttpRequest();
                 //Open our http request as POST with our action variable
                 xmlhttp.open("POST", action, true);
                 xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
                 xmlhttp.onreadystatechange = stateChange;
-                xmlhttp.send(oldPass + '&' + newPass);
-                document.getElementById("passwordRequestError").innerHTML = "Sending password request...";
+                xmlhttp.send(submitData);
+            }
+            
+            function saveNotification() {
+                submitForm('notification');
+            }
+            
+            function savePassword() {
+                submitForm('password');
             }
         </script>
     </head>
@@ -52,8 +72,17 @@
             <hr width="80%" style='margin-top: 0'><br />
             <input class='w3-input' type='password' placeholder='Old Password' id='oldPass' /><br /><br />
             <input class='w3-input' type='password' placeholder='New Password' id='newPass' /><br /><br />
-            <button class='w3-button w3-blue' onclick='submitPasswordRequest()'>Set Password</button><br /><br />
+            <button class='w3-button w3-blue' onclick='savePassword()'>Set Password</button><br /><br />
             <div id="passwordRequestError"></div>
+        </div>
+        <br /><br />
+        <div>
+            <h3 style='margin-bottom: 0'>Notifications</h3>
+            <hr width="80%" style='margin-top: 0'><br />
+            <input class='w3-input' type='email' placeholder='Email Address' id='email' /><br /><br />
+            <input class='w3-check' type='checkbox' /><label>Enable Notifications</label>
+            <button class='w3-button w3-blue' onclick='saveNotification()'>Save Notification Settings</button><br /><br />
+            <div id="notificationError"></div>
         </div>
     </body>
 </html>
