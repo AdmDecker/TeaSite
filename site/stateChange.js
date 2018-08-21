d3e513cea06b4b7d283de2f9038b357b8fc7209a
Adam Decker <deflect57@live.com>
2018-08-21 10:54:55 -0500

    Update stateChange.js

--- site/stateChange.js
+++ site/stateChange.js
@@ -1,3 +1,30 @@
-function stateChange() {
+function stateChange(successCallback, failCallback, debug = false) {
+    return function () {
+        let response = '';
+        try {
+            response = json.decode(xmlhttp.responseText);
+        }
+        catch {
+            console.log('Server error (NOT JSON): ' + xmlhttp.responseText);
+            return;
+        }
+        
+        if (debug) {
+            console.log('RX: ' + response);
+        }
+        
+        //Form variable is REQUIRED
+        const form = response.form;
+        const action = response.action;
     
+        //Handle each action
+        if (response.action === 'redirect')
+            window.location = response.location;
+        else if(response.action === 'error') {
+            successCallback(response.message);
+        }
+        else if(response.action === 'success') {
+            failCallback(response.message);
+        }
+    }
 }
