86f8dfbd4563f50640d02f003d048c6ceba24428
Adam Decker <deflect57@live.com>
2018-08-22 10:29:00 -0500

    Update createdb.sql

--- createdb.sql
+++ createdb.sql
@@ -1,11 +1,12 @@
 CREATE TABLE users
 (
-	userID int NOT NULL AUTO_INCREMENT,
-	userName VARCHAR(255) NOT NULL,
-	password VARCHAR(255) NOT NULL,
+    userID int NOT NULL AUTO_INCREMENT,
+    userName VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     role CHAR,
     teas int,
     timeOfNextOrder int,
-	email VARCHAR(80),
-	PRIMARY KEY (userID)
+    email VARCHAR(80),
+    emailEnabled BOOL,
+    PRIMARY KEY (userID)
 );
