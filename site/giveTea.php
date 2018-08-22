4d6aed0cd7905f5955f2c339539256624fe74c12
Adam Decker <deflect57@live.com>
2018-08-22 15:13:58 -0500

    Update giveTea.php

--- site/giveTea.php
+++ site/giveTea.php
@@ -3,19 +3,27 @@
     require_once('Session.php');
     require_once('PupError.php');
 
-    $e = new PupError('giveTea');
+    $POST = json_decode(file_get_contents('php://input'), true);
 
     if (PupSession::getUserType(PupSession::getUserID()) != 'M')
     {
-        exit();
+        exit( $e->Error('Your session has expired. Please log in again') );
     }
-    
-    $POST = json_decode(file_get_contents('php://input'), true);
 
     $userID = $POST['userID'];
-    $amount = $POST['amount'];    
+    $amount = $POST['amount'];
+    $form = $POST['form']
+    $e = new PupError($form);
 
     $db = new dbAccess();
-    $db->setUserTeas($userID, $amount);
-    echo $e->Success('$userID');
+    $username = '';
+    try {
+        $username = $db->getUsername($userID);
+        $db->setUserTeas($userID, $amount);
+    }
+    catch(PDOException $ex) {
+        exit( $e->Error('Database error: '.$ex->getMessage()) );
+    }
+        
+    echo $e->Success("Successfully set teas for $username to $amount");
 ?>
