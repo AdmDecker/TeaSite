23706f59bbadaf6e1c82ae14542e2870bab79c32
Adam Decker <deflect57@live.com>
2018-08-22 15:17:06 -0500

    Update manager.php

--- site/manager.php
+++ site/manager.php
@@ -73,7 +73,7 @@
             ?>
         </div>
         <br>
-        <label id='giveTeaError' class='center' ></label>
+        <label id='giveTeaError' class='center center-text' ></label>
         <div><button class="w3-button login-input center w3-red" style="margin-top: 50px;" onclick="window.location='/logout.php'">Logout</button></div>
     </body>
 </html>
