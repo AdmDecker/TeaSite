8b207e25b30c64253ebd372ca253c3780b5f85f6
AdmDecker <AdmDecker@gmail.com>
2018-09-02 16:41:33 -0500

    added error handling

--- site/index.php
+++ site/index.php
@@ -3,9 +3,11 @@
     require_once('Session.php');
     require_once('dbaccess.php');
 
+    trigger_error('cooking login');
     //Check for cookie
     if (isset($_COOKIE['loginCookie']))
     {
+        trigger_error($_COOKIE['loginCookie']);
         $db = new dbAccess();
         $userID = $db->getUserByCookie($_COOKIE['loginCookie']);
         if ($userID !== NULL)
