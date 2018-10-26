81b6617997e604a807adb072e158311a15e87a44
AdmDecker <AdmDecker@gmail.com>
2018-10-26 16:15:33 -0500

    removed cookie expiration date

--- site/common.js
+++ site/common.js
@@ -92,10 +92,7 @@ function defaultCallback(formId, message, error)
 }
 
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = cname + "=" + cvalue + ";" + ";path=/";
 }
 
 function redirectPage(newPage) {
